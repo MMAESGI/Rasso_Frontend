271b4fecffbf58891fc915bbde06b4c0bafe1f4f
Alexandre CAROL <sasandre10@gmail.com>
2025-04-24 11:00:06 +0200

    Fix linter & delete useless components/views

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import HeroBanner from '@/components/hero/HeroBanner.vue';
-import TheWelcome from '../components/TheWelcome.vue'
 import PopularEvents from '@/components/events/PopularEvents.vue';
 import TheMap from '@/components/map/TheMap.vue';
 </script>
