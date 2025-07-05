01def4a21d7705c5741befdecb2b9bf57e6ff7d4
Mathis <michenaudmathis@gmail.com>
2025-07-05 19:36:08 +0200

    Client RassoApi

---
+++ src/services/rasso-api.service.ts
@@ -0,0 +1,20 @@
+import { DetailedEventResponseApiResponse, RassoApiClient} from '@mmaesgi/rassoapi-client';
+import jwtAxios from '../lib/axios'; // ← ton axios avec intercepteur JWT
+
+class RassoApiService {
+  private client: RassoApiClient;
+
+  constructor() {
+    this.client = new RassoApiClient('http://localhost:5000', jwtAxios);
+  }
+
+
+  async test(): Promise<void> {
+    const response : DetailedEventResponseApiResponse = await this.client.eventsGET2("id")
+
+  }
+
+
+}
+
+export const rassoApiService = new RassoApiService();
